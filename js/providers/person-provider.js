bc1884fee797ceb86d786ca5fa6e09233f11db68
Lucas Rodrigues Diniz <lucas_diniz99@hotmail.com.br>
2023-02-10 08:55:36 -0300

    primeiro commit

---
+++ js/providers/person-provider.js
@@ -0,0 +1,26 @@
+class PersonProvider {
+    constructor() {      
+    }
+  
+    findById (id) {
+      return { name: 'test'}
+    }
+
+    findByParams (params, value) {
+      return { name: 'test'}
+    }
+
+    save (data) {
+      return { name: 'test'}
+    }
+
+    update (data) {
+      return { name: 'test'}
+    }
+
+    delete (data) {
+      return true
+    }
+    
+  }
+  
